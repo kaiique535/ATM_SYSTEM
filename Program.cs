2ccc9834e5e2d3b902d3cff1c0b8d8d521d8cc21
Jonathan Victorino <82481212+kaiique535@users.noreply.github.com>
2022-12-06 12:47:14 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,112 +1,34 @@
-﻿using System;
+﻿//Focus on the development solution, where the user just enters a value
+//and the application returns the arrangement of the notes. 
+//The system must: Validate the input, return best arrangement notes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program
+namespace new_atm_system
 {
-    static void BankNoteSelection(int withdrawValue)
+    class Program
     {
-        var availableBankNotes = new int[] { 200, 100, 50, 20, 10 };
 
-        if (withdrawValue == 0 || withdrawValue < 0)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Please, entry with a value more than zero");
-            //Verifica se o numero é par valido para saque
-        }
-        else if (withdrawValue == 0)
-            Console.WriteLine(" successfull withdraw! ");
-        else if (withdrawValue % 2 == 0)
-        {
-            int[] bankNotes = new int[availableBankNotes.Length];
+            Withdraw withdraw = new Withdraw();
+            System.Console.WriteLine("Enter withdraw value:");
+            withdraw.WithdrawValue = int.Parse(Console.ReadLine());
+            var test = withdraw.BanknotesSelection(withdraw.WithdrawValue);
+            withdraw.PrintResult(test);
 
-            for (var item = 0; item < availableBankNotes.Length; item++)
-            {
-                bankNotes[item] = (int)(
-                    Math.Floor((decimal)withdrawValue / availableBankNotes[item])
-                );
-                withdrawValue -= (bankNotes[item] * availableBankNotes[item]);
-            }
-            for (int item = 0; item < bankNotes.Length; item++)
-                Console.WriteLine($"Cedula de {availableBankNotes[item]}: {bankNotes[item]}");
+
         }
 
-        return;
     }
 
-    public static void Main(string[] args)
-    {
-        int balanceAmount = 5000;
-        int withdrawValue = 0;
-        bool openApp = true;
-        int deposit;
-
-        bool isValidNumber;
-        int operation;
-
-        while (openApp)
-        {
-            Console.WriteLine("");
-            Console.WriteLine("Welcome to the electronic cashout system!");
-            Console.WriteLine("Select the desired operation below:");
-            Console.WriteLine("1 - WITHDRAW");
-            Console.WriteLine("2 - DEPOSIT");
-            Console.WriteLine("3 - BALANCE");
-            Console.WriteLine("4 - LOGOUT");
-
-            string operationEntry = Console.ReadLine();
-
-            isValidNumber = int.TryParse(operationEntry, out operation);
-
-            if ((isValidNumber == false) || (operation <= 0) || (operation > 4))
-            {
-                Console.WriteLine("Plese entry with a valid operation number");
-            }
-
-            switch (operation)
-            {
-                case 1:
-                    string bankNotes;
-                    Console.WriteLine("Enter with withdraw value:");
-
-                    string withdrawValueEntry = Console.ReadLine();
-                    isValidNumber = int.TryParse(withdrawValueEntry, out withdrawValue);
-
-                    if (withdrawValue % 2 != 0)
-                    {
-                        Console.WriteLine("Please entry with a valid value");
-                    }
-                    if (withdrawValue > balanceAmount)
-                    {
-                        Console.WriteLine("Insufficient balance for withdrawal");
-                    }
-                    if (
-                        (isValidNumber == true)
-                        && (withdrawValue > 0)
-                        && (withdrawValue <= balanceAmount)
-                    )
-                    {
-                        balanceAmount = balanceAmount - withdrawValue;
-                        //bankNotes = bankNoteSelection(withdrawValue);
-                        Program notes = Program();
-                        notes.BankNoteSelection();
-                        Console.WriteLine("Your withdrawal is complete!");
-                        Console.WriteLine($"Your current balance is: {balanceAmount}");
-                    }
-                    break;
-                case 2:
-                    Console.WriteLine("Whats the deposit value");
-                    deposit = int.Parse(Console.ReadLine());
-
-                    balanceAmount = balanceAmount + deposit;
-
-                    Console.WriteLine($"Your current balance is: R${balanceAmount}");
-
-                    break;
-                case 3:
-                    Console.WriteLine($"Your balance is: R${balanceAmount}");
-                    break;
-                case 4:
-                    openApp = false;
-                    break;
-            }
-        }
-    }
 }
+
+//Comentarios ronny : Fazer a tabela verdade das entradas e saidas para saber definir a logica.
+//Tudo que eu acesso frequentemente deve ficar modular com chamada para onde for usar.
+//Sempre conhecer as entradas e saidas antes de começar a resolver o problema.
+
+
---
+++ Withdraw.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace new_atm_system
+{
+    internal class Withdraw
+    {
+        public int WithdrawValue { get; set; }
+        int[] AvailableBankNotes = new int[] { 200, 100, 50, 20, 10, 5, 2 };//Array of available bank notes.
+        Notas testNewStruct = new Notas();
+        eNotas DescriptionNote;
+        int Quantidade { get; set; }
+        public enum eNotas//Enum to identify the output of the program. When we ask for the withdrawal amount,
+                          //the computer doesn't know which ballot it is, just numbers. Enum sorts what the output is. 
+        {
+            eNota2 = 2,
+            eNota5 = 5,
+            eNota10 = 10,
+            eNota20 = 20,
+            eNota50 = 50,
+            eNota100 = 100,
+            eNota200 = 200,
+        }
+        public struct Notas//Will create a struct to be added on list of withdraw
+        {
+            public eNotas Nota { get; set; }// To identify the value type of banknotes
+            public int Quantidade { get; set; }//To identify a quantity of banknotes to be withdrawn
+        }
+        public List<Notas> BanknotesSelection(int WithdrawValue)//Method to create list of results by withdraw
+        {
+            List<Notas> result = new List<Notas>();//Creating List of banknotes available.
+            for (int i = 0; i < AvailableBankNotes.Length; i++)
+            {
+                if (WithdrawValue >= AvailableBankNotes[i])
+                {
+                    if (WithdrawValue % 5 == 1 || WithdrawValue % 5 == 3)
+                    {
+                        WithdrawValue -= 6;
+                        DescriptionNote = eNotas.eNota2;
+                        testNewStruct.Nota = DescriptionNote;
+                        testNewStruct.Quantidade = 3;
+                        result.Add(testNewStruct);
+
+                    }
+                    int quantityBankNotes = WithdrawValue / AvailableBankNotes[i];
+                    WithdrawValue -= (quantityBankNotes * AvailableBankNotes[i]);
+
+                    DescriptionNote = (eNotas)AvailableBankNotes[i];
+                    testNewStruct.Nota = DescriptionNote;
+                    testNewStruct.Quantidade = quantityBankNotes;
+
+                    if (testNewStruct.Quantidade != 0)
+                    {
+                        result.Add(testNewStruct);
+                    }
+                    if (WithdrawValue == 0)//check if withdrawal amount is greater than zero to break the loop
+                    {
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+        public void PrintResult(List<Notas> result)
+        {
+            foreach (var item in result)
+            {
+                Console.WriteLine($"Saque: {item.Nota} | Quantidade = {item.Quantidade}");
+
+            }
+        }
+    }
+}
